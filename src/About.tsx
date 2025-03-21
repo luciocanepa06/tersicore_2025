e85bae916db9d24e0de555d0f863baef189fc165
luciocanepa <lucio.canepa.6@gmail.com>
2025-03-21 14:48:49 +0100

    updated about page2

--- src/About.tsx
+++ src/About.tsx
@@ -23,7 +23,7 @@ function About() {
           <div className='about-text'>
             <p>
             Nel novembre del 2002 cinque amici poco più che ventenni, che avevano già cantato insieme per una decina d’anni nel medesimo coro, hanno deciso di riunirsi nuovamente e di fondare l’Associazione Coro Tersicore. A muoverli non era solo l’intenzione di condividere un’attività artistica, ma soprattutto il desiderio di condividere la gioia del canto corale inteso come esperienza comune di vita e di crescita. Tra le muse tradizionalmente associate al canto si è dunque preferita la Tersicore per la sua immagine di ispiratrice e protettrice del canto corale e della danza collettiva. 
-            
+            <br></br>
             Mentre il Coro Tersicore cominciava ad esibirsi nei primi concerti a partire da un repertorio polifonico di musica classica prevalentemente cinque-seicentesca, il numero di coristi aumentava di pari passo con gli impegni, contando sull’apporto decisivo di persone motivate e coinvolte. Sin dalle origini i coristi hanno dato la propria disponibilità ad animare anche le funzioni religiose, cogliendo nella bellezza del canto l’elemento unificante tra musica sacra e musica profana. 
 
             </p>
@@ -34,7 +34,8 @@ function About() {
 
             <p>
             La versatilità riconosciuta al gruppo gli ha permesso di animare varie messe in occasione di matrimoni e di feste patronali, a cui va aggiunta la messa per l’Ascensione trasmessa in Eurovisione dalla RSI nel 2010, anno nel quale ha anche collaborato alla pubblicazione di un CD in memoria di Luigi Picchi. Nel 2013, a distanza di dieci anni dalla sua fondazione, il Coro Tersicore ha pubblicato il CD Dieci, con opere di Felix Mendelssohn Bartholdy. Nel 2014 ha partecipato alla XXI edizione del Festival internazionale di musica corale “D.G. KIRIAC”, svoltosi nella regione della Muntenia (Romania), esibendosi nelle sezioni “musica corale romena ed internazionale” e “musica sacra”. Nel 2019 il Coro è stato invitato a partecipare alla Rassegna Corale Europea dei Popoli in Friuli. 
-            
+            <br></br>
+            <br></br>
             Il Coro Tersicore si è esibito in collaborazione con diversi cori e orchestre, tra cui la Scuola Corale Scarlatti di Bollate (MI), La Scuola Corale della Cattedrale, il Coro Alpe Adria di Treppo Grande (UD), il Coro Les Courants d’Airs (Parigi), il Gruppo Canzoni e Costumi Ticinesi di Bellinzona, i Cantori di Pregassona, la Filarmonica Mosaico di Monte Carasso, l’Orchestra da Camera di Lugano e l’ensemble Hnoss di Milano.
 
             </p>
